c20441d8d1426124a21bc0c57950e5e1196e5e5c
igorfonseca05 <igorfondev@gmail.com>
2025-11-17 19:31:13 -0300

    corrigi cors

--- server.ts
+++ server.ts
@@ -30,7 +30,8 @@ app.use(express.json());
 // -------------- CORS CORRIGIDO --------------
 app.use(
   cors({
-    origin: "https://csv-to-json-converter-rouge.vercel.app",
+    // origin: "https://csv-to-json-converter-rouge.vercel.app",
+    origin: "https://csv-json-backend-production.up.railway.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
